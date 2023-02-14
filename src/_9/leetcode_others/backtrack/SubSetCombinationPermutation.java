b461e8f75d91ed643afdec2a4847df94a0e663bd
steven_chan <stevenchang0038@gmail.com>
2023-02-14 16:10:36 +0800

    update

--- src/_9/leetcode_others/backtrack/SubSetCombinationPermutation.java
+++ src/_9/leetcode_others/backtrack/SubSetCombinationPermutation.java
@@ -7,7 +7,9 @@ import java.util.List;
 /**
  * general backtrack template of subsets/permutations/combinations
  * <p>
- * refer from http://pglost.github.io/blog-theme/leetcode/2015/08/10/leetcode-subset.html
+ * refer from
+ * 1. http://pglost.github.io/blog-theme/leetcode/2015/08/10/leetcode-subset.html
+ * 2. https://segmentfault.com/a/1190000040142137
  */
 public class SubSetCombinationPermutation {
     public static void main(String[] args) {
@@ -19,11 +21,33 @@ public class SubSetCombinationPermutation {
         System.out.println(scp.subsetsWithDup(nums2));
         System.out.println(scp.permute(nums));
         System.out.println(scp.permuteUnique(nums2));
+
+        System.out.println(scp.combine(4, 2));
+
+        /*
+         - 全排列 (Permutation)
+                - N 個元素取 N 個元素 (所有元素) 的所有排列組合情況
+                - 無重複序列排列
+                - 有重複序列排列
+         - 組合  (Combinations)
+                - n 個元素取 m 個元素的所有組合情況 (非排列)
+                - 組合只看元素不看順序
+         - 子集  (Subsets)
+                - 組合的擴展, 不考慮排列順序先後
+                - n 個元素的所有子集 (所有可能的組合情況)
+                - 重複子集
+                - 不重複子集
+         */
     }
 
 
     /**
      * 1-1. 子集: 給定一個不重複的整數數組, 返回其所有的子集
+     * <p>
+     * Input: nums = [1,2,3]
+     * Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+     *
+     * @see Subsets
      */
     public List<List<Integer>> subsets(int[] nums) {
         Arrays.sort(nums); // 題目中要求子集中的元素為非降序排列
@@ -36,9 +60,10 @@ public class SubSetCombinationPermutation {
     }
 
     // 從 nums 的第 i 個元素開始, 計算包含 sub 的所有子集, 並保存到 subs 中
-    private void subsetsHelper(int[] nums, int pos, List<Integer> sub, List<List<Integer>> subs) {
+    private void subsetsHelper(int[] nums, int posIdx, List<Integer> sub, List<List<Integer>> subs) {
         subs.add(new ArrayList<>(sub));
-        for (int i = pos; i < nums.length; i++) { // 實際上是解決子問題, 從 i 開始的每一個元素保存到 sub 中
+
+        for (int i = posIdx; i < nums.length; i++) { // 實際上是解決子問題, 從 i 開始的每一個元素保存到 sub 中
             sub.add(nums[i]);
             subsetsHelper(nums, i + 1, sub, subs);
             sub.remove(sub.size() - 1);
@@ -47,6 +72,8 @@ public class SubSetCombinationPermutation {
 
     /**
      * 1-2. 子集: 給定一個可能有重複數字的數組, 返回其所有的子集
+     *
+     * @see SubSetII
      */
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         Arrays.sort(nums);
@@ -58,11 +85,11 @@ public class SubSetCombinationPermutation {
         return subs;
     }
 
-    private void subsetsWithDupHelper(int[] nums, int pos, List<Integer> sub, List<List<Integer>> subs) {
+    private void subsetsWithDupHelper(int[] nums, int posIdx, List<Integer> sub, List<List<Integer>> subs) {
         subs.add(new ArrayList<>(sub));
 
-        for (int i = pos; i < nums.length; i++) {
-            if (i != pos && nums[i] == nums[i - 1]) {
+        for (int i = posIdx; i < nums.length; i++) {
+            if (i != posIdx && nums[i] == nums[i - 1]) {
                 continue;
             }
 
@@ -74,6 +101,8 @@ public class SubSetCombinationPermutation {
 
     /**
      * 2-1 排列: 全排列 , 給定一個數組, 返回其所有可能的排列
+     *
+     * @see Permutations
      */
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
@@ -100,6 +129,8 @@ public class SubSetCombinationPermutation {
 
     /**
      * 2-2 排列: 帶重複元素的排列 , 給定一個具有重複數字的數組, 返回其所有可能的排列
+     *
+     * @see PermutationsII
      */
     public List<List<Integer>> permuteUnique(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
@@ -132,26 +163,27 @@ public class SubSetCombinationPermutation {
 
 
     /**
-     * 3-1 組合:
+     * 3-1 組合: 給定二個整數 n 和 k, 返回 1..n 中所有可能的 k 個數的組合
      */
     public List<List<Integer>> combine(int n, int k) { // Cn 取 k
-        List<List<Integer>> results = new ArrayList<>(); // 结果
-        int nums[] = new int[n]; // 数组存储 1-n
-        boolean visited[] = new boolean[n]; // 用于判断是否使用
-        for (int i = 0; i < n; i++) {
+        List<List<Integer>> results = new ArrayList<>();
+        boolean[] visited = new boolean[n]; // 用於判斷是否使用
+        int[] nums = new int[n];
+        for (int i = 0; i < n; i++) { // init nums 1..n
             nums[i] = i + 1;
         }
 
-        dfs(-1, k, results, visited, n);
+        dfs(nums, -1, k, results, visited, n);
         return results;
     }
 
-    private void dfs(int index, int count, List<List<Integer>> results, boolean visited[], int n) {
-        if (count == 0) { // k个元素满
+    private void dfs(int[] nums, int idx, int count, List<List<Integer>> results, boolean[] visited, int n) {
+        if (count == 0) { // k 個元素滿
             List<Integer> list = new ArrayList<>();
             for (int i = 0; i < n; i++) {
                 if (visited[i]) {
-                    list.add(i + 1);
+//                    list.add(i + 1);
+                    list.add(nums[i]);
                 }
             }
             results.add(list);
@@ -159,10 +191,10 @@ public class SubSetCombinationPermutation {
             return;
         }
 
-        for (int i = index + 1; i < n; i++) { // 只能在index后遍历 回溯向下
+        for (int i = idx + 1; i < n; i++) { // 只能在 index 後遍歷,回朔向下
             visited[i] = true;
-            dfs(i, count - 1, results, visited, n);
-            visited[i] = false; //还原
+            dfs(nums, i, count - 1, results, visited, n);
+            visited[i] = false; // 還原 backtrack
         }
     }
 
