6b5eaa3ffbb0149ad559ff76e7ed16a97a1f6d6f
stevenchang0038 <stevenchang0038@gmail.com>
2023-05-24 17:09:58 -0400

    Java basic APIs init

---
+++ src/_0/UtileLibraries/IterableExamples/CustomIterableClient.java
@@ -0,0 +1,20 @@
+package _0.UtileLibraries.IterableExamples;
+
+/**
+ * ref. https://www.baeldung.com/java-iterator-vs-iterable
+ */
+class CustomIterableClient {
+
+    public static void main(String[] args) {
+
+        ShoppingCart<Product> shoppingCart = new ShoppingCart<>();
+        shoppingCart.add(new Product("Tuna", 42));
+        shoppingCart.add(new Product("Eggplant", 65));
+        shoppingCart.add(new Product("Salad", 45));
+        shoppingCart.add(new Product("Banana", 29));
+
+        for (Product product : shoppingCart) {
+            System.out.println(product.getName());
+        }
+    }
+}
