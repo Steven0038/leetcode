20795e48ccbb9e8df9090dfe0cc56db1f9e68294
stevenchang0038 <stevenchang0038@gmail.com>
2023-06-26 11:49:48 -0400

    update

--- src/_9/leetcode_others/heap/KthLargestElementsInAnArray.java
+++ src/_9/leetcode_others/heap/KthLargestElementsInAnArray.java
@@ -1,6 +1,7 @@
 package _9.leetcode_others.heap;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.PriorityQueue;
 
 /**
@@ -38,47 +39,69 @@ public class KthLargestElementsInAnArray {
     }
 
     /**
-     * - 1. 維護一個遞減的 Heap(Priority Queue), 讓 heap 中的元素不超過 k 個
-     * - 2. 只加進比 heap 的 root 大 的元素
-     * - 3. 同時滿足 1&2 二點, heap 的 root 永遠是當前所有 heap 元素中第 k 大的元素(而且是動態的)
-     * - 所有 array 都做完的話, heap 的 root 就是 array 中第 K 大的元素
-     * <p>
-     * O(n * log n)
+     * Solution1 30%, 偷吃步
      *
      * @param nums input array
      * @param k    kth largest
      * @return the kth largest element in input array
      */
-    private Integer findKthLargest(int[] nums, int k) {
-        PriorityQueue<Integer> heap = new PriorityQueue<>();
-        for (int num : nums) {
-            if (heap.isEmpty()) heap.offer(num); // TODO
+    public int findKthLargest(int[] nums, int k) {
+        Arrays.sort(nums);
 
-            if (!heap.isEmpty() && (heap.size() < k || num >= heap.peek())) {
-                heap.offer(num);
-            }
-
-            if (heap.size() > k) {
-                heap.poll();
-            }
+        for (int i = nums.length - 1; i >= 0; i--) {
+            k = k - 1;
+            if (k == 0) return nums[i];
         }
 
-        return heap.peek();
+        return 0;
     }
 
+    /**
+     * Solution1 13%, 最大堆 heap 直觀作法
+     */
+    public int findKthLargest1(int[] nums, int k) {
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Comparator.reverseOrder()); // 降序
+        for (int n : nums) {
+            maxHeap.add(n);
+        }
+
+        int count = k;
+        int ret = 0;
+        while (count != 0 && !maxHeap.isEmpty()) {
+            ret = maxHeap.poll();
+            count = count - 1;
+        }
+
+        return ret;
+    }
+
+    /**
+     * 32%
+     * - 1. 維護一個遞減的 minHeap(Priority Queue), 讓 heap 中的元素不超過 k 個
+     * 在上述代码中，我们使用了Java的PriorityQueue来实现最小堆。我们遍历数组中的每个元素，并将其插入到最小堆中。如果堆的大小超过了 k，则将堆顶元素（最小值）弹出。最终，堆顶元素即为第 k 个最大的元素。
+     * <p>
+     * 时间复杂度分析：插入和弹出操作的时间复杂度为O(logk)，在最坏情况下，需要处理数组中的所有元素，因此时间复杂度为O(nlogk)，其中 n 是数组的长度。
+     * <p>
+     * 空间复杂度分析：最小堆中最多包含 k 个元素，因此空间复杂度为O(k)。
+     * <p>
+     * 通过使用最小堆，我们可以高效地找出数组中第 k 个最大的元素。
+     * <p>
+     * O(n * log n)
+     */
     public int findKthLargest2(int[] nums, int k) {
         if (nums.length == 0) return 0;
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
 
-        PriorityQueue<Integer> heap = new PriorityQueue<>();
-        for (int n : nums) {
-            heap.offer(n);
+        for (int num : nums) {
+            minHeap.offer(num);
 
-            if (heap.size() > k) {
-                heap.poll();
+            if (minHeap.size() > k) {
+                minHeap.poll();
             }
         }
 
-        if (heap.isEmpty()) return 0;
-        return heap.peek();
+        if (minHeap.isEmpty()) return 0;
+
+        return minHeap.peek();
     }
 }
